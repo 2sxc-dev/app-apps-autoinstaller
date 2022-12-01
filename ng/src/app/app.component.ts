39122c8101d2d42cc420774ac5cd88542098ea6b
Dominik Graf <dominik.graf@2sic.com>
2022-12-01 13:52:21 +0100

    wip: url params and layout DNN

--- ng/src/app/app.component.ts
+++ ng/src/app/app.component.ts
@@ -17,17 +17,26 @@ export class AppComponent extends SxcAppComponent {
   apps$!: Observable<Apps[]>; //alle apps als observable, greifen hier im html mit async darauf zurück
   selectedApps!: BehaviorSubject<Apps[]>; //hilfe observable anzahl in html anzuzeigen
 
-  selectedAppsArr : Apps[] = []; // array welches wir mit ausgewählten Apps befühlen / ist kein observable
-
+  selectedAppsArr: Apps[] = []; // array welches wir mit ausgewählten Apps befühlen / ist kein observable
   isTilesView: boolean = true; // view von tile zu list je nach dem andere klassen einblenden, beim start tile
-
+  appLenght: boolean = false;
   private isAllSelected!: BehaviorSubject<boolean>; // observable mit true oder false der selecte box auszuwählten / abzuwählen
 
+  params = new URLSearchParams(window.location.search);
+  sxcVersion = this.params.get("sxcversion");
+  sysversion = this.params.get("sysversion");
+  sexyContentVersion = this.params.get("SexyContentVersion");
+  moduleId = this.params.get("ModuleId");
+  hasUrlParams = true;
+
   ngOnInit(): void {
+
+    this.hasUrlParams = this.params.has("sysversion") && this.params.has("sxcversion") && this.params.has("SexyContentVersion");
+
     this.isAllSelected = new BehaviorSubject(true); // zuerst sind alle checkboxen ausgewählt und true
     this.selectedApps = new BehaviorSubject([]); //hier werden die ausgewählten elemente hinzugefügt
 
-    this.dataService.getApp().pipe(
+    this.apps$ = this.dataService.getApp(this.sxcVersion, this.sysversion, this.sexyContentVersion, this.moduleId).pipe(
       combineLatestWith(this.isAllSelected), // abhänigkeit von apps$ ist das Mockup APPS und die selected
       map(([apps, allSelected]) => {
         this.selectedAppsArr = []; // erstelle ein leeres Array
@@ -35,29 +44,19 @@ export class AppComponent extends SxcAppComponent {
         apps.forEach(app => {
           app.isSelected = allSelected; //übergib der app den status, ob es isSelected ist oder nicht, zubeinn sind alle selected
 
-          if(app.isSelected) { // wenn app selectet ist
-            this.selectedAppsArr.push(app) // füge die app in das Arry hinzu wenn es ausgewählt wird
+          console.log(app.isSelected)
+
+          if (app.isSelected == true) {
+            this.appLenght = true
           }
-        });
+          if (app.isSelected == '') {
+            this.appLenght = false
+          }
 
-        apps.sort((a, b) => a.displayName.localeCompare(b.displayName)) // Sotieren von A - Z
+          console.log(this.appLenght, 're')
 
-        return apps; // gib die apps mit dem neuen status in die apps$ zurück auf die wir später zugreiffen
-      }),
-      delay(0), // lösst problem von change detection
-      tap(() => {
-        this.selectedApps.next(this.selectedAppsArr); // füge das array in das behaviorsubjcet zu, so dass es verändernungen mit bekommt
-      })
-    )
-    this.apps$ = this.dataService.getApp().pipe(
-      combineLatestWith(this.isAllSelected), // abhänigkeit von apps$ ist das Mockup APPS und die selected
-      map(([apps, allSelected]) => {
-        this.selectedAppsArr = []; // erstelle ein leeres Array
 
-        apps.forEach(app => {
-          app.isSelected = allSelected; //übergib der app den status, ob es isSelected ist oder nicht, zubeinn sind alle selected
-
-          if(app.isSelected) { // wenn app selectet ist
+          if (app.isSelected) { // wenn app selectet ist
             this.selectedAppsArr.push(app) // füge die app in das Arry hinzu wenn es ausgewählt wird
           }
         });
@@ -74,7 +73,7 @@ export class AppComponent extends SxcAppComponent {
   }
 
   changeValue(app: Apps, urlKey: string) { // über gibt die app und die urlKey
-    const found = this.selectedAppsArr.some((app : Apps) => app.urlKey == urlKey); // finde die app, bei der die app.urlKey und urlkey gleich sind
+    const found = this.selectedAppsArr.some((app: Apps) => app.urlKey == urlKey); // finde die app, bei der die app.urlKey und urlkey gleich sind
 
     if (!found) {
       this.selectedAppsArr.push(app) // pusht die neue app in das Array und es wir vergrössert
@@ -90,7 +89,7 @@ export class AppComponent extends SxcAppComponent {
   }
 
   installApps() {
-    var message = { 'action': 'install', 'moduleId': 'window.ModuleId', 'packages': this.selectedAppsArr}; // werte die übergeben werden wie z.b. die ausgewählten Apps
+    var message = { 'action': 'install', 'moduleId': 'window.ModuleId', 'packages': this.selectedAppsArr }; // werte die übergeben werden wie z.b. die ausgewählten Apps
     window.parent.postMessage(JSON.stringify(message), '*'); // message wir an das übergeortente fenster weitergeben / der code wird mit Iframe eingebunden
   }
 
--- ng/src/app/services/data.service.ts
+++ ng/src/app/services/data.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
@@ -8,21 +10,11 @@ export class DataService {
 
   constructor(private http: HttpClient) { }
 
-  params = new URLSearchParams(window.location.search);
+  getApp(sxcVersion: string, sysversion: string, sexyContentVersion: string, moduleId: string): any {
+    const url = `https://2sxc.org/en/api/2sxc/app/App-Installer/api/AppInstaller/GetAutoInstallerApps?ModuleId=${moduleId}&SexyContentVersion=${sexyContentVersion}&platform=Dnn&sysversion=${sysversion}&sxcversion=${sxcVersion}`
+    return this.http.get(url).pipe(tap(data => console.log(data)))
+  }
+}
 
-  sxcVersion = this.params.get("sxcversion");
-  sysversion = this.params.get("sysversion");
-  SexyContentVersion = this.params.get("SexyContentVersion");
-  ModuleId = this.params.get("ModuleId");
 
   //https://2sxc.org/en/2sic-funktionstestseite/autoinstaller-test?ModuleId=1199&SexyContentVersion=13.11.00&platform=Dnn&sysversion=9.1.1&sxcversion=11.07.03
-  //https://2sxc.org/en/2sic-funktionstestseite/autoinstaller-test?ModuleId=1199&SexyContentVersion=13.11.00&platform=Dnn&sysversion=9.9.1&sxcversion=13.11.00
-
-  private url: string = `https://2sxc.org/en/api/2sxc/app/App-Installer/api/AppInstaller/GetAutoInstallerApps?ModuleId=${this.ModuleId}&SexyContentVersion=${this.SexyContentVersion}&platform=Dnn&sysversion=${this.sysversion}&sxcversion=${this.sxcVersion}`
-  // private url: string = '/Portals/0/2sxc/App-Installer/ng/src/assets/data.json'
-
-  getApp(): any {
-    return this.http.get(this.url)
-  }
-
-}
